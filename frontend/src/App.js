c64178d495672d4d00813549cbc7fd88ac191b57
samuelashraff <48100109+samuelashraff@users.noreply.github.com>
2023-09-26 13:17:24 +0300

    Created react project in frontend DIR

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import StudyList from './components/StudyList';
+
+function App() {
+  return (
+    <div className="root">
+      <h1>Hello World</h1>
+      <StudyList/>
+    </div>
+  );
+}
+
+export default App;
