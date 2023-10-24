592ebec0dafc81a4036c29d83568dfa5808ffb08
Röttgen, Valentin <valentin.roettgen@adesso.de>
2023-10-24 17:11:50 +0300

    make course cards same height

--- packages/frontend/src/components/CourseCard.tsx
+++ packages/frontend/src/components/CourseCard.tsx
@@ -4,12 +4,12 @@ const CourseCard: React.FC<{ course: { cname: string; status: string } }> = ({
   course,
 }) => {
   return (
-    <Card className="max-w-xs rounded overflow-ellipsis shadow-lg">
-      <CardHeader>
+    <Card className="max-w-xs rounded overflow-ellipsis shadow-lg h-40 flex flex-col">
+      <CardHeader className="h-2/3">
         <CardTitle>{course.cname}</CardTitle>
       </CardHeader>
-      <CardContent>
-        <div>Status: {course.status}</div>
+      <CardContent className="h-1/3">
+        <p className="overflow-hidden">Status: {course.status}</p>
       </CardContent>
     </Card>
   );
