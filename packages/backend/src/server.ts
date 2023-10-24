434ed101164f6ef640e4033acc3cabfbc488e650
Joel Hassan <joel.nhn@gmail.com>
2023-10-24 18:59:12 +0300

    use main router for displaying courses

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -1,12 +1,40 @@
 import Koa from "koa";
 import cors from "@koa/cors";
+import Router from "koa-router";
+
+
+// routes: ADD THESE AS NEEDED
+// main (/) -> course list
+// course (/course/:id)
+// assignment (/assignment/:id)
+// lecture (/lecture/:id)
+
+const courses = [
+  { id: 100, cname: "Design of WWW Services", status: "In progress" },
+  { id: 101, cname: "Calculus 1", status: "Not started" },
+  { id: 102, cname: "WWW Applications", status: "In progress" },
+  {
+    id: 103,
+    cname:
+      "Social theory of finance: the giving and taking of value in the financialisation of our lives",
+    status: "Completed",
+  },
+];
+
+const rootRouter = new Router({
+  prefix: "/",
+});
+
+
+rootRouter.get("/", async (ctx, next) => {
+  ctx.body = courses;
+  next();
+});
 
 const app = new Koa();
 
 app.use(cors());
-
-let courses = require("./courses.ts");
-app.use(courses.routes());
+app.use(rootRouter.routes());
 
 const PORT = process.env.PORT || 4000;
 
