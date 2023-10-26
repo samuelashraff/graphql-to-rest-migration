70db916dfd4b4ec50f1b254dbfe239f1fb8f00d1
Röttgen, Valentin <valentin.roettgen@adesso.de>
2023-10-26 15:31:11 +0300

    initialise sqlite database

---
+++ packages/backend/src/database/data.sql
@@ -0,0 +1,9 @@
+-- Insert a single dummy user
+INSERT INTO users (name, password) VALUES ('John Doe', 'password123');
+
+-- Insert dummy courses
+INSERT INTO courses (cname, credits, user_id, status, notes, start_date, end_date, responsible_teacher, location, course_link) VALUES
+('Design of WWW Services', 3, 1, 'in progress', 'Some notes here', '2023-10-01', '2024-01-15', 'Dr. Smith', 'Building A, Room 101', 'https://example.com/course1'),
+('Calculus 1', 4, 1, 'not started', 'No notes yet', NULL, NULL, NULL, NULL, NULL),
+('WWW Applications', 3, 1, 'in progress', 'Check the schedule for details', '2023-09-15', '2023-12-20', 'Prof. Johnson', 'Building B, Room 205', 'https://example.com/course2'),
+('Social theory of finance: the giving and taking of value in the financialisation of our lives', 5, 1, 'done', 'Important concepts covered', '2023-08-20', '2023-12-10', 'Dr. Williams', 'Online', 'https://example.com/course3');
--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -1,8 +1,9 @@
 import Koa from "koa";
 import cors from "@koa/cors";
 import Router from "koa-router";
+import db from "./database/db";
 
-
+const sqliteDB = db();
 // routes: ADD THESE AS NEEDED
 // main (/) -> course list
 // course (/course/:id)
@@ -25,9 +26,25 @@ const rootRouter = new Router({
   prefix: "/",
 });
 
-
 rootRouter.get("/", async (ctx, next) => {
-  ctx.body = courses;
+  try {
+    const rows = await new Promise((resolve, reject) => {
+      sqliteDB.all(`SELECT * FROM courses`, [], (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+
+    ctx.body = rows;
+    ctx.status = 200;
+  } catch (error) {
+    console.error("Error while querying the database:", error);
+    ctx.status = 500;
+    ctx.body = { error: "Internal Server Error" };
+  }
   next();
 });
 
