d62d2733346ebe3b5197cb4dce04b96093e76786
Joel Hassan <joel.nhn@gmail.com>
2023-11-12 21:44:00 +0200

    clean up timetable

--- packages/frontend/src/components/TimeTable.tsx
+++ packages/frontend/src/components/TimeTable.tsx
@@ -1,5 +1,5 @@
-import { BASE_URL } from '@/config';
-import { useState, useEffect } from 'react'
+import { BASE_URL } from "@/config";
+import { useState, useEffect } from "react";
 
 type TimeTableItem = {
     deadline: string;
@@ -9,60 +9,59 @@ type TimeTableItem = {
     isObligatory: boolean;
 };
 
-export function TimeTable() {
-    const [data, setData] = useState<TimeTableItem[]>([])
+// Helper function to calculate days until an event
+function calculateDaysUntilEvent(eventDate: string): number {
+    const today = new Date();
+    const deadlineDate = new Date(eventDate);
+    const timeDifference = deadlineDate.getTime() - today.getTime();
+    const daysUntilDeadline = Math.ceil(timeDifference / (1000 * 3600 * 24));
+    return daysUntilDeadline;
+}
 
-    // Helper function to calculate days until an event
-    function calculateDaysUntilEvent(eventDate: string): number {
-        const today = new Date();
-        const deadlineDate = new Date(eventDate);
-        const timeDifference = deadlineDate.getTime() - today.getTime();
-        const daysUntilDeadline = Math.ceil(timeDifference / (1000 * 3600 * 24));
-        return daysUntilDeadline;
-    }
+const Event = ({ item, index }: { item: TimeTableItem; index: number }) => {
+    const isAssignment = item.deadline ? true : false;
+    const title = isAssignment ? "Assignment" : "Lecture";
+    const daysUntilEvent = isAssignment
+        ? calculateDaysUntilEvent(item.deadline)
+        : calculateDaysUntilEvent(item.date);
 
+    return (
+        <li key={index} className="border border-black rounded-md p-4 mt-4">
+            <>
+                <p className="font-bold">
+                    {title} {index + 1}
+                </p>
+                <p>Deadline: {new Date(item.deadline).toLocaleDateString()}</p>
+                <p>({daysUntilEvent} days left)</p>
+            </>
+        </li>
+    );
+};
 
+export function Timetable() {
+    const [data, setData] = useState<TimeTableItem[]>([]);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch(`${BASE_URL}/timetable`)
-                const timeTableData = await response.json()
-                setData(timeTableData)
+                const response = await fetch(`${BASE_URL}/timetable`);
+                const timeTableData = await response.json();
+                setData(timeTableData);
+            } catch (error) {
+                console.error("Error fetching timetable data" + error);
             }
-            catch (error) {
-                console.error("Error fetching timetable data" + error)
-            }
-        }
-        fetchData()
-        console.log("Timetable data: " + data)
-    }, [])
+        };
+        fetchData();
+    }, [data]);
 
     return (
         <div className="flex flex-col">
             <h1 className="text-2xl font-bold">Upcoming Events: </h1>
-
-            {data.map((item, index) => (
-                <div
-                    key={index}
-                    className="border border-black rounded-md p-4 mt-4"
-                >
-                    {item.deadline ? ( // Check if it's an assignment
-                        <>
-                            <p className="font-bold">Assignment {index + 1}</p>
-                            <p>Deadline: {new Date(item.deadline).toLocaleDateString()}</p>
-                            <p>({calculateDaysUntilEvent(item.deadline)} days left)</p>
-                        </>
-                    ) : ( // It's a lecture
-                        <>
-                            <p className="font-bold">Lecture {index + 1}</p>
-                            <p>Location: {item.location}</p>
-                            <p>Date: {new Date(item.date).toLocaleDateString()}</p>
-                            <p>({calculateDaysUntilEvent(item.date)} days left)</p>
-                        </>
-                    )}
-                </div>
-            ))}
+            <ul className="overflow-auto max-h-[50vh]">
+                {data.map((item, index) => (
+                    <Event key={index} item={item} index={index} />
+                ))}
+            </ul>
         </div>
-    )
+    );
 }
